0ff1ed83a4c724cfe0b01e61013c2d3b34b41b4d
artfer <artureco95.af@gmail.com>
2019-11-27 18:23:05 +0000

    many erros. please fix

--- eco.c
+++ eco.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define loc(mat,i,j,C) mat[ (i * C) + j ] 
 
 typedef struct {
   int type;           // 0 -> rock ,1 -> rabbit, 2 -> fox
@@ -12,6 +12,8 @@ typedef struct {
   int state;          // 0 -> Dead, 1 -> Alive
 } Object;
 
+int* matrix, moves;
+Object *objects;
 
 int str_to_num(char str[]){
     if(!strcmp(str,"ROCK"))
@@ -22,7 +24,7 @@ int str_to_num(char str[]){
         return 2;    
 }
 
-int num_to_char(int x){
+char num_to_char(int x){
     if(x == -1)
         return ' ';
     else if(x == 0)
@@ -32,6 +34,25 @@ int num_to_char(int x){
     else 
         return 'F';    
 }
+
+char* num_to_str(int x){
+    if(x == 0)
+        return "ROCK";
+    else if (x == 1)
+        return "RABBIT";
+    else 
+        return "FOX";    
+}
+
+
+void prepare_move(int n){
+    if(objects[n].state == 0){
+        moves[n] = -1;
+        return;
+    }
+}
+
+
 
 int main(){
 
@@ -51,13 +72,13 @@ int main(){
     scanf("%d",&C);
     scanf("%d",&N);
 
-    int matrix[R][C];
+    matrix = malloc( R * C * sizeof(int));
     for(int i = 0; i < R; i++)
         for(int j = 0; j < C; j++)
-            matrix[i][j] = -1;
+            loc(matrix,i,j,C) = -1;
 
 
-    Object *objects = (Object *) malloc(N * sizeof(Object));
+    objects = (Object *) malloc(N * sizeof(Object));
 
     int ani_start;
 
@@ -69,7 +90,7 @@ int main(){
 
         objects[i].type = str_to_num(tmp_type);
 
-        matrix[objects[i].i][objects[i].j] = objects[i].type;
+        loc(matrix,objects[i].i,objects[i].j,C) = objects[i].type;
 
         if(objects[i].type == 0)
             ani_start = i;
@@ -78,23 +99,25 @@ int main(){
 
     ani_start++;
 
-    /*
+    
     for(int i = 0; i < R; i++){
         for(int j = 0; j < C; j++)
-            printf("%c ", num_to_char(matrix[i][j]))    ;
+            printf("%c", num_to_char(loc(matrix,i,j,C)));
         printf("\n");
     }
-
+    
+    /*
     for(int i=0; i < N; i++)
         printf("%d %d %d\n", objects[i].type, objects[i].i, objects[i].j);
     */
-
+    
     int i,j;
-    int moves[N];
+    moves = (int *)malloc(N * sizeof(int));
 
     for(int gen=0; gen < N_GEN; gen++){
         
-        for(int n = ani_start; n < N; n++){
+        int n;
+        for(n = ani_start; n < N; n++){
             
             if (objects[n].state == 0)
                 continue;
@@ -104,24 +127,62 @@ int main(){
 
             if(matrix[i-1][j] == -1){
                 moves[n] = 0;
-                break;
             } else if(matrix[i][j+1] == -1) {
                 moves[n] = 1;
-                break;
             } else if(matrix[i+1][j] == -1){
                 moves[n] = 2;
-                break;
             } else if(matrix[i][j-1] == -1){
                 moves[n] = 3;
-                break;
             } else {
                 moves[n] = -1;
-                break;
             }
+        }
+
+        for(n = ani_start; n < N; n++){
             
+            if (objects[n].state == 0 || moves[n] == -1)
+                continue;
+
+            i = objects[n].i;
+            j = objects[n].j;
+
+            if(moves[n] == 0){ 
+                if(matrix[i-1][j] != -1){
+                    
+                } else {
+                    matrix[i][j] == -1;
+                    matrix[i-1][j] == objects[n].type;
+                    objects[n].i--;
+                }
+                continue; 
+            } else if(moves[n] == 1){
+                if(matrix[i][j+1] != -1){
+                    
+                } else {
+                    matrix[i][j] == -1;
+                    matrix[i][j+1] == objects[n].type;
+                    objects[n].j++;
+                }
+            } else if(moves[n] == 2){
+                if(matrix[i+1][j] != -1){
+                    
+                } else {
+                    matrix[i][j] == -1;
+                    matrix[i+1][j] == objects[n].type;
+                    objects[n].i++;
+                }
+            } else if(moves[n] == 1){
+                if(matrix[i][j-1] != -1){
+                    
+                } else {
+                    matrix[i][j] == -1;
+                    matrix[i][j-1] == objects[n].type;
+                    objects[n].j--;
+                }
+            } 
+
         }
 
     }
-
 
 }
