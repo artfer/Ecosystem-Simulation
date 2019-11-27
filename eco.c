e5ba62a8efd76a0547674457b3f936fd0fa5bb03
artfer <artureco95.af@gmail.com>
2019-11-27 23:30:55 +0000

    removed type and added id in the struct

--- eco.c
+++ eco.c
@@ -2,17 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define EMPTY -2147483648
 #define loc(mat,i,j,C) mat[ (i * C) + j ] 
 
 typedef struct {
-  int type;           // 0 -> rock ,1 -> rabbit, 2 -> fox
-  int i;              // position i in the enviroment
-  int j;              // position j in the enviroment
-  int gen_food;       // generations passed without eating
-  int state;          // 0 -> Dead, 1 -> Alive
+    int id;             // id of object (rocks are all 0)
+    int i;              // position i in the enviroment
+    int j;              // position j in the enviroment
+    int gen_food;       // generations passed without eating
+    int gen_proc;       // generations passed without procreating
+    int state;          // 0 -> Dead, 1 -> Alive
 } Object;
 
-int* matrix, moves;
+int* matrix;
+int* moves;
+int GEN_PROC_RABBITS; // number of generations until a rabbit can procreate
+int GEN_PROC_FOXES;   // number of generations until a fox can procreate
+int GEN_FOOD_FOXES;   // number of generations for a fox to die of starvation
+int N_GEN;            // number of generations for the simulation
+int R;                // number of rows of the matrix representing the ecosystem
+int C;                // number of columns of the matrix representing the ecosystem
+int N;                // number of objects in the initial ecosystem
 Object *objects;
 
 int str_to_num(char str[]){
@@ -25,11 +35,11 @@ int str_to_num(char str[]){
 }
 
 char num_to_char(int x){
-    if(x == -1)
+    if(x == EMPTY)
         return ' ';
     else if(x == 0)
         return '*';
-    else if (x == 1)
+    else if (x > 1)
         return 'R';
     else 
         return 'F';    
@@ -44,25 +54,66 @@ char* num_to_str(int x){
         return "FOX";    
 }
 
+void print_mat(){
+    int tmp;
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            tmp = loc(matrix,i,j,C); 
+            if(tmp == EMPTY)
+                printf(" ");
+            else if(tmp < 0)
+                printf("F");
+            else if(tmp == 0)
+                printf("*");
+            else
+                printf("R");
+        }
+        printf("\n");
+    }
+}
+
 
 void prepare_move(int n){
     if(objects[n].state == 0){
         moves[n] = -1;
         return;
     }
+
+    int i = objects[n].i;
+    int j = objects[n].j;
+
+    //if fox, search for rabbit
+    if(objects[n].id < 0){
+        if(loc(matrix,i-1,j,C) > 0){
+            moves[n] = 0;
+            return;
+        } else if(loc(matrix,i,j+1,C) > 0){
+            moves[n] = 1;
+            return;
+        } else if(loc(matrix,i+1,j,C) > 0){
+            moves[n] = 2;
+            return;
+        } else if(loc(matrix,i,j-1,C) > 0){
+            moves[n] = 3;
+            return;
+        }
+    }
+
+    if(loc(matrix,i-1,j,C) == EMPTY)
+        moves[n] = 0;
+    else if(loc(matrix,i,j+1,C) == EMPTY)
+        moves[n] = 1;
+    else if(loc(matrix,i+1,j,C) == EMPTY)
+        moves[n] = 2;
+    else if(loc(matrix,i,j-1,C) == EMPTY)
+        moves[n] = 3;
+    else
+        moves[n] = -1;
 }
 
 
 
 int main(){
-
-    int GEN_PROC_RABBITS; // number of generations until a rabbit can procreate
-    int GEN_PROC_FOXES;   // number of generations until a fox can procreate
-    int GEN_FOOD_FOXES;   // number of generations for a fox to die of starvation
-    int N_GEN;            // number of generations for the simulation
-    int R;                // number of rows of the matrix representing the ecosystem
-    int C;                // number of columns of the matrix representing the ecosystem
-    int N;                // number of objects in the initial ecosystem
 
     scanf("%d",&GEN_PROC_RABBITS);
     scanf("%d",&GEN_PROC_FOXES);
@@ -75,12 +126,10 @@ int main(){
     matrix = malloc( R * C * sizeof(int));
     for(int i = 0; i < R; i++)
         for(int j = 0; j < C; j++)
-            loc(matrix,i,j,C) = -1;
+            loc(matrix,i,j,C) = EMPTY;
 
 
     objects = (Object *) malloc(N * sizeof(Object));
-
-    int ani_start;
 
     for(int i=0; i < N; i++){
 
@@ -88,101 +137,46 @@ int main(){
 
         scanf("%s %d %d", tmp_type, &objects[i].i, &objects[i].j);
 
-        objects[i].type = str_to_num(tmp_type);
+        
 
-        loc(matrix,objects[i].i,objects[i].j,C) = objects[i].type;
+        int tmp = str_to_num(tmp_type);
 
-        if(objects[i].type == 0)
-            ani_start = i;
+        objects[i].id = tmp == 0 ? 0 : tmp == 1 ? i : i*(-1); 
+
+        loc(matrix,objects[i].i,objects[i].j,C) = objects[i].id;
 
     }
-
-    ani_start++;
 
     
-    for(int i = 0; i < R; i++){
-        for(int j = 0; j < C; j++)
-            printf("%c", num_to_char(loc(matrix,i,j,C)));
-        printf("\n");
-    }
+    print_mat();
     
     /*
     for(int i=0; i < N; i++)
         printf("%d %d %d\n", objects[i].type, objects[i].i, objects[i].j);
     */
-    
-    int i,j;
+    /*
+
     moves = (int *)malloc(N * sizeof(int));
 
     for(int gen=0; gen < N_GEN; gen++){
         
-        int n;
-        for(n = ani_start; n < N; n++){
+        for(int n = 0; n < N; n++){
             
-            if (objects[n].state == 0)
+            if (objects[n].type == 0 || objects[n].state == 0)
                 continue;
 
-            i = objects[n].i;
-            j = objects[n].j;
-
-            if(matrix[i-1][j] == -1){
-                moves[n] = 0;
-            } else if(matrix[i][j+1] == -1) {
-                moves[n] = 1;
-            } else if(matrix[i+1][j] == -1){
-                moves[n] = 2;
-            } else if(matrix[i][j-1] == -1){
-                moves[n] = 3;
-            } else {
-                moves[n] = -1;
-            }
+            prepare_move(n);
         }
 
-        for(n = ani_start; n < N; n++){
+        for(int n = 0; n < N; n++){
             
-            if (objects[n].state == 0 || moves[n] == -1)
+            if (objects[n].type == 0 || objects[n].state == 0 || moves[n] == -1)
                 continue;
-
-            i = objects[n].i;
-            j = objects[n].j;
-
-            if(moves[n] == 0){ 
-                if(matrix[i-1][j] != -1){
-                    
-                } else {
-                    matrix[i][j] == -1;
-                    matrix[i-1][j] == objects[n].type;
-                    objects[n].i--;
-                }
-                continue; 
-            } else if(moves[n] == 1){
-                if(matrix[i][j+1] != -1){
-                    
-                } else {
-                    matrix[i][j] == -1;
-                    matrix[i][j+1] == objects[n].type;
-                    objects[n].j++;
-                }
-            } else if(moves[n] == 2){
-                if(matrix[i+1][j] != -1){
-                    
-                } else {
-                    matrix[i][j] == -1;
-                    matrix[i+1][j] == objects[n].type;
-                    objects[n].i++;
-                }
-            } else if(moves[n] == 1){
-                if(matrix[i][j-1] != -1){
-                    
-                } else {
-                    matrix[i][j] == -1;
-                    matrix[i][j-1] == objects[n].type;
-                    objects[n].j--;
-                }
-            } 
 
         }
 
     }
+
+    */
 
 }
