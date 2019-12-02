fb0c003cb9cece37f60c409c70599ae6cafb7e3d
artfer <artureco95.af@gmail.com>
2019-12-02 21:39:12 +0000

    need prepare move first

--- eco.c
+++ eco.c
@@ -3,8 +3,18 @@
 #include <string.h>
 
 #define EMPTY -2147483648
-#define loc(mat,i,j,C) mat[ (i * C) + j ] 
-#define valid(i,j) i < 0 ? 0 : i > R ? 0 : j < 0 ? 0 : j > C ? 0 : 1   
+#define loc(i,j) matrix[ (i * C) + j ] 
+#define valid(i,j) i < 0 ? 0 : i > R ? 0 : j < 0 ? 0 : j > C ? 0 : 1
+#define North_f(i,j) valid(i-1,j) && loc(i-1,j) > 0
+#define East_f(i,j) valid(i,j+1) && loc(i,j+1) > 0
+#define South_f(i,j) valid(i+1,j) && loc(i+1,j) > 0
+#define West_f(i,j) valid(i,j-1) && loc(i,j-1) > 0
+#define North(i,j) valid(i-1,j) && loc(i-1,j) == EMPTY
+#define East(i,j) valid(i,j+1) && loc(i,j+1) == EMPTY
+#define South(i,j) valid(i+1,j) && loc(i+1,j) == EMPTY
+#define West(i,j) valid(i,j-1) && loc(i,j-1) == EMPTY
+#define Get_i(p,i) p == 0 ? i-1 : p == 2 ? i+1 : i
+#define Get_j(p,j) p == 1 ? j+1 : p == 3 ? j-1 : j  
 
 typedef struct {
     int id;             // id of object (rocks are all 0)
@@ -69,7 +79,7 @@ void print_mat(int g){
     for(int i = 0; i < R; i++){
         printf("|");
         for(int j = 0; j < C; j++){
-            tmp = loc(matrix,i,j,C); 
+            tmp = loc(i,j); 
             if(tmp == EMPTY)
                 printf(" ");
             else if(tmp < 0)
@@ -93,7 +103,7 @@ void new_object(int t, int i,int j){
             rabbits = (Object *) realloc(rabbits,size_rab * sizeof(Object));
         }
         rabbits[n_rab].id = n_rab + 1;
-        loc(matrix,i,j,C) = rabbits[n_rab].id;
+        loc(i,j) = rabbits[n_rab].id;
         rabbits[n_rab].i = i;
         rabbits[n_rab].j = j;
         rabbits[n_rab].gen_proc = 0;
@@ -105,7 +115,7 @@ void new_object(int t, int i,int j){
             foxes = (Object *) realloc(foxes,size_fox * sizeof(Object));
         }
         foxes[n_fox].id = (n_fox + 1) * -1;
-        loc(matrix,i,j,C) = foxes[n_fox].id;
+        loc(i,j) = foxes[n_fox].id;
         foxes[n_fox].i = i;
         foxes[n_fox].j = j;
         foxes[n_fox].gen_proc = 0;
@@ -128,28 +138,28 @@ void prepare_move(int n){
 
     //if fox, search for rabbit
     if(objects[n].id < 0){
-        if(loc(matrix,i-1,j,C) > 0){
+        if(loc(matrix,i-1,j) > 0){
             moves[n] = 0;
             return;
-        } else if(loc(matrix,i,j+1,C) > 0){
+        } else if(loc(matrix,i,j+1) > 0){
             moves[n] = 1;
             return;
-        } else if(loc(matrix,i+1,j,C) > 0){
+        } else if(loc(matrix,i+1,j) > 0){
             moves[n] = 2;
             return;
-        } else if(loc(matrix,i,j-1,C) > 0){
+        } else if(loc(matrix,i,j-1) > 0){
             moves[n] = 3;
             return;
         }
     }
 
-    if(loc(matrix,i-1,j,C) == EMPTY)
+    if(loc(matrix,i-1,j) == EMPTY)
         moves[n] = 0;
-    else if(loc(matrix,i,j+1,C) == EMPTY)
+    else if(loc(matrix,i,j+1) == EMPTY)
         moves[n] = 1;
-    else if(loc(matrix,i+1,j,C) == EMPTY)
+    else if(loc(matrix,i+1,j) == EMPTY)
         moves[n] = 2;
-    else if(loc(matrix,i,j-1,C) == EMPTY)
+    else if(loc(matrix,i,j-1) == EMPTY)
         moves[n] = 3;
     else
         moves[n] = -1;
@@ -157,74 +167,52 @@ void prepare_move(int n){
 */
 
 
-void make_move(int n, int t){
+void make_move(int n, int t, Object *cur){
     
-    Object cur;
-    if(t == 1)
-        cur = rabbits[n];
-    else
-        cur = foxes[n];
-    
-    if(cur.state == 0){
+    if(cur->state == 0){
         return;
     }
 
-    int i = cur.i;
-    int j = cur.j;
-    int dest = -1;
-    int dest_i = -1;
-    int dest_j = -1;
-    int count = 0;
+    int move[4];
+    int c = 0;
 
     //if fox, search for rabbit
-    if(t == 2){
-        if(valid(i-1,j) && loc(matrix,i-1,j,C) > 0){
-            dest = 0;
-            count++; 
-        } 
-        if(valid(i,j+1) && loc(matrix,i,j+1,C) > 0){
-            dest = 1;
-            count++;
-        } 
-        if(valid(i+1,j) && loc(matrix,i+1,j,C) > 0){
-            dest = 2;    
-            count++;
-        }
-        if(valid(i,j-1) && loc(matrix,i,j-1,C) > 0){
-            dest = 3;
-            count++;
+    if(cur->id < 0){
+        if(North_f(cur->i,cur->j)){
+             move[c] = 0; c++;
+        } if(East_f(cur->i,cur->j)){
+            move[c] = 1; c++;
+        } if(South_f(cur->i,cur->j)){
+            move[c] = 2; c++;
+        } if(West_f(cur->i,cur->j)){
+            move[c] = 3; c++;
         }
     }
-    if(dest == -1){
-        if(valid(i-1,j) && loc(matrix,i-1,j,C) == EMPTY){
-            dest = 0;
-            count++;
+    if(c == 0){
+        if(North(cur->i,cur->j)){
+             move[c] = 0; c++;
+        } if(East(cur->i,cur->j)){
+            move[c] = 1; c++;
+        } if(South(cur->i,cur->j)){
+            move[c] = 2; c++;
+        } if(West(cur->i,cur->j)){
+            move[c] = 3; c++;
         }
-        if(valid(i,j+1) && loc(matrix,i,j+1,C) == EMPTY){
-            dest = 1;
-            count++;
-        }
-        if(valid(i+1,j) && loc(matrix,i+1,j,C) == EMPTY){
-            dest = 2;
-            count++;
-        }
-        if(valid(i,j-1) && loc(matrix,i,j-1,C) == EMPTY){
-            dest = 3;
-            count++;
-        }
-        if(count == 0){
-            cur.moved = 0;
+        if(c == 0){
+            cur->moved = 0;
             return;
         }
     }
 
-    if(count > 1){
-        dest = (gen + i + j) % count;
-        printf("%d %d %d\n",i,j,dest);
+    if(c == 1){
+        loc(cur->i,cur->j) = EMPTY;
+        cur->i = Get_i(move[0],cur->i);
+        cur->j = Get_j(move[0],cur->j);
+        loc(cur->i, cur->j) = cur->id;
     }
     /*
-    loc(matrix,i,j,C) = EMPTY;
-    loc(matrix,dest_i,dest_j,C) = cur.id;
+    loc(matrix,i,j) = EMPTY;
+    loc(matrix,dest_i,dest_j) = cur.id;
     cur.gen_proc++;
     if(t == 1 && cur.gen_proc % GEN_PROC_RABBITS == 0){
         
@@ -249,7 +237,7 @@ int main(){
     matrix = malloc( R * C * sizeof(int));
     for(int i = 0; i < R; i++)
         for(int j = 0; j < C; j++)
-            loc(matrix,i,j,C) = EMPTY;
+            loc(i,j) = EMPTY;
 
 
     foxes = (Object *) malloc(N * sizeof(Object));
@@ -271,7 +259,7 @@ int main(){
         if(tmp != 0)
             new_object(tmp, i,j);
         else
-            loc(matrix,i,j,C) = 0;
+            loc(i,j) = 0;
 
     }
 
@@ -287,10 +275,10 @@ int main(){
         print_mat(gen);
         
         for(i = 0; i < n_rab; i++)
-            make_move(i,1);
+            make_move(i, 1, &rabbits[i]);
 
         for(i = 0; i < n_fox; i++)
-            make_move(i,2);
+            make_move(i, 2, &foxes[i]);
 
     }
 
